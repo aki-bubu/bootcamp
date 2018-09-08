def46e3d2b316903a2e2dd180740251ad08fadca
晟煕 菅谷 <akihiro.ghf@gmail.com>
2018-09-08 16:15:23 +0900

    9/8 Activity画面実装

--- SANTE_0.01/js/main.js
+++ SANTE_0.01/js/main.js
@@ -1,7 +1,35 @@
 $(function(){
     'use strict';
     
-    
+    //datapicker
+    let dateFormat ='yy-mm-dd';
+    $('#datepicker').datepicker({
+        dateFormat: dateFormat
+    });
+   
+   
+    window.onload = function () {
+        //今日の日時を表示
+        var date = new Date()
+        var year = date.getFullYear()
+        var month = date.getMonth() + 1
+        var day = date.getDate()
+      
+        var toTwoDigits = function (num, digit) {
+          num += ''
+          if (num.length < digit) {
+            num = '0' + num
+          }
+          return num
+        }
+        
+        var yyyy = toTwoDigits(year, 4)
+        var mm = toTwoDigits(month, 2)
+        var dd = toTwoDigits(day, 2)
+        var ymd = yyyy + "-" + mm + "-" + dd;
+        
+        document.getElementById("datepicker").value = ymd;
+    }
     // 最初の表示一覧
 
     $.getJSON("json/activity.json",
