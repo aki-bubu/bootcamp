fabbebf3fe072b9d21b2bacd4f5b7d921cf6ea85
晟煕 菅谷 <akihiro.ghf@gmail.com>
2018-09-09 16:07:28 +0900

    9/9 jsonファイルの構成修正済み

--- SANTE_0.01/js/main.js
+++ SANTE_0.01/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     'use strict';
     
-    //datapicker
+    //datepicker
     let dateFormat ='yy-mm-dd';
     $('#datepicker').datepicker({
         dateFormat: dateFormat
@@ -32,54 +32,32 @@ $(function(){
     }
     // 最初の表示一覧
 
-    $.getJSON("json/activity.json",
-        function(data){
-                for(var i=0; i< data.fun.length; i++){
-                    let container = $('<a href="detail.html"></a>');
+    let fun=[];
+    let relax=[];
+    let hard=[];
+    function makeContainers(item){
+            $.getJSON("/json/activity.json",
+                function pushes(data){
+                    for (let i = 0; i < data.length; i++) {
+                        if(data[i].key === 'fun') {
+                            fun.push(data[i]);
+                        }else if(data[i].key ==='relax'){
+                            relax.push(data[i]);
+                        }else{
+                            hard.push(data[i]);
+                        }
+                    }
+                    // let i =0;
+                for(var i=0; i< item.length; i++){
+                    let container = $('<a href="detail?activity_id="1".html"></a>');
+                //    console.log(item[i].id);
                     let imageContainer = $('<div></div>');
                     let timeContainer = $('<div></div>');
                     let profileContainer = $('<div></div>');
                     let detailContainer = $('<div></div>');
                     let locationContainer = $('<div><img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"></div>');
-                    let location =$(' <img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"><p>'+ data.fun[i].location +'</p>');
-                    
-                    $(container).addClass("container");
-                    $(imageContainer).addClass("image-container");
-                    $(timeContainer).addClass("time-container");
-                    $(profileContainer).addClass("profile-container");
-                    $(detailContainer).addClass("detail-container");
-                    $(locationContainer).addClass("location-container");
-                    $(location).addClass("location");
-                    //ここの画像ファイルとテキストを入れる形に
-                    imageContainer.html('<img class ="container-img"src="'+ data.fun[i].image +'" alt="' + data.fun[i].name + '">');
-                    timeContainer.html('<p>'+ data.fun[i].time +'</p>');
-                    profileContainer.html('<img src="' + data.fun[i].profile +'" alt="プロフィール">');
-                    detailContainer.html('<p>'+ data.fun[i].name +'</p>');
-                    location.html()
-                    locationContainer.html(location);
-                
-                    container.html(imageContainer);
-                    container.append(timeContainer).append(profileContainer).append(detailContainer).append(locationContainer);
-                    $(".main-container").prepend(container);
-                    // // return container;
-            
-        };
-    });
+                    let location =$(' <img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"><p>'+ item[i].location +'</p>');
 
-    $('#slide-title1').on("click", function(){
-        
-        $('.main-container').empty();
-        $.getJSON("json/activity.json",function(data){           
-                // container =[];
-                for(var i=0; i< data.fun.length; i++){
-                    let container = $('<a href="detail.html"></a>');
-                    let imageContainer = $('<div></div>');
-                    let timeContainer = $('<div></div>');
-                    let profileContainer = $('<div></div>');
-                    let detailContainer = $('<div></div>');
-                    let locationContainer = $('<div><img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"></div>');
-                    let location =$(' <img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"><p>'+ data.fun[i].location +'</p>');
-                    
                     $(container).addClass("container");
                     $(imageContainer).addClass("image-container");
                     $(timeContainer).addClass("time-container");
@@ -88,102 +66,31 @@ $(function(){
                     $(locationContainer).addClass("location-container");
                     $(location).addClass("location");
                     //ここの画像ファイルとテキストを入れる形に
-                    imageContainer.html('<img class ="container-img"src="'+ data.fun[i].image +'" alt="' + data.fun[i].name + '">');
-                    timeContainer.html('<p>'+ data.fun[i].time +'</p>');
-                    profileContainer.html('<img src="' + data.fun[i].profile +'" alt="プロフィール">');
-                    detailContainer.html('<p>'+ data.fun[i].name +'</p>');
+                    imageContainer.html('<img class ="container-img"src="'+ item[i].image +'" alt="' + item[i].name + '">');
+                    timeContainer.html('<p>'+ item[i].time +'</p>');
+                    profileContainer.html('<img src="' + item[i].profile +'" alt="プロフィール">');
+                    detailContainer.html('<p>'+ item[i].name +'</p>');
                     location.html()
                     locationContainer.html(location);
-                
                     container.html(imageContainer);
                     container.append(timeContainer).append(profileContainer).append(detailContainer).append(locationContainer);
                     $(".main-container").prepend(container);
-                    // // return container;
-                
-            };
-        });
-    });
-    $('#slide-title2').on("click", function(){
-        // let container=[];
-        $('.main-container').empty();
-        $.getJSON("json/activity.json",function(data){            
-                for(var i=0; i< data.relax.length; i++){                       
-                    let container = $('<a href="detail.html"></a>');
-                    let imageContainer = $('<div></div>');
-                    let timeContainer = $('<div></div>');
-                    let profileContainer = $('<div></div>');
-                    let detailContainer = $('<div></div>');
-                    let locationContainer = $('<div><img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"></div>');
-                    let location =$(' <img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"><p>'+ data.fun[i].location +'</p>');
-                    
-                    $(container).addClass("container");
-                    $(imageContainer).addClass("image-container");
-                    $(timeContainer).addClass("time-container");
-                    $(profileContainer).addClass("profile-container");
-                    $(detailContainer).addClass("detail-container");
-                    $(locationContainer).addClass("location-container");
-                    $(location).addClass("location");
-                    //ここの画像ファイルとテキストを入れる形に
-                    imageContainer.html('<img class ="container-img"src="'+ data.relax[i].image +'" alt="' + data.relax[i].name + '">');
-                    timeContainer.html('<p>'+ data.relax[i].time +'</p>');
-                    profileContainer.html('<img src="' + data.relax[i].profile +'" alt="プロフィール">');
-                    detailContainer.html('<p>'+ data.relax[i].name +'</p>');
-                    location.html()
-                    locationContainer.html(location);
-                
-                    container.html(imageContainer);
-                    container.append(timeContainer).append(profileContainer).append(detailContainer).append(locationContainer);
-                    $(".main-container").prepend(container);
-                    // // return container;
-                
-            };
-        });
-    });
-
-        $('#slide-title3').on("click", function(){
-            // let container =[];
-            $('.main-container').empty();
-            $.getJSON("json/activity.json",function(data){                  
-                    for(var i=0; i< data.hard.length; i++){
-                        let container = $('<a href="detail.html"></a>');
-                        let imageContainer = $('<div></div>');
-                        let timeContainer = $('<div></div>');
-                        let profileContainer = $('<div></div>');
-                        let detailContainer = $('<div></div>');
-                        let locationContainer = $('<div><img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"></div>');
-                        let location =$(' <img class="location-icon" src="icon/cont_location_icon.png" alt="アイコン"><p>'+ data.fun[i].location +'</p>');
-                        
-                        $(container).addClass("container");
-                        $(imageContainer).addClass("image-container");
-                        $(timeContainer).addClass("time-container");
-                        $(profileContainer).addClass("profile-container");
-                        $(detailContainer).addClass("detail-container");
-                        $(locationContainer).addClass("location-container");
-                        $(location).addClass("location");
-                        //ここの画像ファイルとテキストを入れる形に
-                        imageContainer.html('<img class ="container-img"src="'+ data.hard[i].image +'" alt="' + data.hard[i].name + '">');
-                        timeContainer.html('<p>'+ data.hard[i].time +'</p>');
-                        profileContainer.html('<img src="' + data.hard[i].profile +'" alt="プロフィール">');
-                        detailContainer.html('<p>'+ data.hard[i].name +'</p>');
-                        location.html()
-                        locationContainer.html(location);
-                    
-                        container.html(imageContainer);
-                        container.append(timeContainer).append(profileContainer).append(detailContainer).append(locationContainer);
-                        $(".main-container").prepend(container);
-                        // return container;
-                    
-                };
+                    // console.log(relax);
+                    // console.log(hard);
+                }
             });
-        
-    });
-    
-    
-});
-    
-        
-    
-    
+    }
+    makeContainers(fun);
 
-    
+
+    $('#slide-title1').on("click", function(){ 
+        makeContainers(fun);
+    });
+    $('#slide-title2').on("click", function(){ 
+        makeContainers(relax);
+    });
+    $('#slide-title3').on("click", function(){ 
+        makeContainers(hard);
+    });
+});
     
